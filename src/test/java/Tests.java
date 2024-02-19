8cad8473bffdec9c91c4cb4efe8ac491b30024d9
ARIADNA <rariadna@inbox.ru>
2024-02-19 15:00:26 +0300

    BookingServiceTest

---
+++ src/test/java/Tests.java
@@ -0,0 +1,50 @@
+import java.BookingService;
+import java.CantBookException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.LocalDateTime;
+import static org.mockito.ArgumentMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class Tests {
+    private static final Logger logger
+            = LoggerFactory.getLogger(Tests.class);
+
+    @Mock
+    BookingService bookingService = new BookingService();
+    @Test
+    void positiveBookingTest() throws CantBookException {
+        logger.info("запуск positiveBookingTest");
+        Mockito.when(bookingService.book(anyString(), any(LocalDateTime.class), any(LocalDateTime.class)))
+                .thenReturn(true);
+        Assertions.assertEquals(true, bookingService
+                .book("User", LocalDateTime.now(), LocalDateTime.now()));
+        Mockito.verify(bookingService, Mockito.times(1)).book(anyString()
+                , any(LocalDateTime.class)
+                , any(LocalDateTime.class));
+        Mockito.verifyNoMoreInteractions(bookingService);
+        logger.info("Тест positiveBookingTest прошёл успешно");
+    }
+    @Test
+    void negativeBookingTest() throws CantBookException {
+        //given
+        logger.info("запуск negativeBookingTest");
+        Mockito.doThrow(CantBookException.class)
+                .when(bookingService).book(anyString(), any(LocalDateTime.class), any(LocalDateTime.class));
+        //then
+        Assertions.assertThrows(CantBookException.class, () -> bookingService
+                .book("User", LocalDateTime.now(), LocalDateTime.now()));
+
+        Mockito.verify(bookingService, Mockito.times(1)).book(anyString()
+                , any(LocalDateTime.class)
+                , any(LocalDateTime.class));
+        Mockito.verifyNoMoreInteractions(bookingService);
+        logger.info("Тест negativeBookingTest прошёл успешно");
+    }
+}
